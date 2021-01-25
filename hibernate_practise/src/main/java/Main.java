fbabf670f0bc809c145a087661bc8363bea1be99
ValenbergHar <maskevich.valeri@gmail.com>
2021-01-25 16:37:01 +0300

    25.01.21

--- hibernate_practise/src/main/java/Main.java
+++ hibernate_practise/src/main/java/Main.java
@@ -1,9 +1,16 @@
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    //private static List<Person> personList;
     public static void main(String[] args) {
+
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Person.class)
@@ -16,7 +23,16 @@ public class Main {
         Session session = null;
 
         try{
+            session = factory.getCurrentSession();
+            session.beginTransaction();
 
+            Person person= (Person) session.createQuery("FROM Person p WHERE p.id =2");
+         //   personList.add(person);
+            System.out.println(person);
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
         }
 
     }
