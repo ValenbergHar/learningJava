765565c89329a27301ec82b44e75da6b127215d1
ValenbergHar <maskevich.valeri@gmail.com>
2021-01-28 23:09:21 +0300

    28.01.21

--- hibernate/app1/src/main/java/HelloWorldJPA.java
+++ hibernate/app1/src/main/java/HelloWorldJPA.java
@@ -2,6 +2,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 public class HelloWorldJPA {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
@@ -14,8 +16,9 @@ public class HelloWorldJPA {
 
         session = factory.getCurrentSession();
         session.beginTransaction();
-        Message message = new Message();
-        message.setText("Hello World!");
+        Message message = new Message("Zianon");
+//        Message message = new Message();
+       // message.setText("Stas");
         session.save(message);
         session.getTransaction().commit();
 
--- hibernate/app1/src/main/java/Message.java
+++ hibernate/app1/src/main/java/Message.java
@@ -1,22 +1,35 @@
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "app1")
 public class Message {
     @Id
     @GeneratedValue
-    private Long id;
+    private int id;
 
     private String text;
+
+    @Temporal(TemporalType.DATE)
+    @CreationTimestamp
+    private Date date;
 
     public String getText() {
         return text;
     }
 
     public void setText(String text) {
+        this.text = text;
+    }
+
+    public Message(String text, Date date) {
+        this.text = text;
+        this.date = date;
+    }
+
+    public Message(String text) {
         this.text = text;
     }
 }
