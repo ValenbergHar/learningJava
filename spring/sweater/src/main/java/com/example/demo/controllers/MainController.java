583f48e562f03eabd0252d18af63a7e10268f0a6
ValenbergHar <maskevich.valeri@gmail.com>
2021-04-07 16:57:34 +0300

    07.04.21

--- spring/sweater/src/main/java/com/example/demo/controllers/MainController.java
+++ spring/sweater/src/main/java/com/example/demo/controllers/MainController.java
@@ -6,16 +6,21 @@ import com.example.demo.repos.MessageRepo;
 import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -29,6 +34,7 @@ public class MainController {
     public String greeting(Map<String, Object> model) {
         return "hello";
     }
+
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
@@ -48,32 +54,38 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam("file") MultipartFile file,
-            @RequestParam String text, @RequestParam String tag,
-            Map<String, Object> model
+            @Valid Message message,
+            BindingResult bindingResult,
+            Model model
     ) throws IOException {
-        Message message = new Message(text, tag, user);
+        message.setAuthor(user);
 
-        if (file != null && !file.getOriginalFilename().isEmpty()) {
-            File uploadDir = new File(uploadPath);
+        if (bindingResult.hasErrors()) {
+            bindingResult.getFieldErrors().stream().collect();
+        } else {
 
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
+
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+                File uploadDir = new File(uploadPath);
+
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+                file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+                message.setFilename(resultFilename);
             }
 
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-            message.setFilename(resultFilename);
+            messageRepo.save(message);
         }
-
-        messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
         return "main";
     }
-
 
 
 }
