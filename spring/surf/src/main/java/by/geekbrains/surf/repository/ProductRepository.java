b7f79e98462c7b24197bc65cd6cd48a6a9505340
ValenbergHar <maskevich.valeri@gmail.com>
2021-04-13 15:42:29 +0300

    13.04.21

--- spring/surf/src/main/java/by/geekbrains/surf/repository/ProductRepository.java
+++ spring/surf/src/main/java/by/geekbrains/surf/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package by.geekbrains.surf.repository;
 
 import by.geekbrains.surf.entity.Product;
+import by.geekbrains.surf.entity.ProductType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    public List<Product> findByProductType(ProductType productType);
 }
